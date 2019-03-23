34f38365618e0a855cbef20254abec2f991a1ea3
chen <1367973836@qq.com>
2019-03-23 22:10:13 +0800

    将配置中心同时作为服务注册中心使用

--- spring-cloud-config-server/src/main/java/com/scau/config/ConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/scau/config/ConfigServerApplication.java
@@ -3,16 +3,16 @@ package com.scau.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * 配置中心
+ * 配置中心同时也是服务中心
  * 
  * @author chen
  * @date 2019/03/15
  *
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
